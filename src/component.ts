a74e8af957fcef73d7e4cc5eec472543630cda17
Lautaro Petaccio <lausuper@gmail.com>
2023-03-08 14:11:22 -0300

    feat: Support logging unhandled errors

--- src/component.ts
+++ src/component.ts
@@ -15,7 +15,7 @@ export function instrumentHttpServerWithRequestLogger(
   const inLogger = logger.getLogger('http-in')
   const outLogger = logger.getLogger('http-out')
 
-  server.use((ctx: IHttpServerComponent.DefaultContext<object>, next) => {
+  server.use(async (ctx: IHttpServerComponent.DefaultContext<object>, next) => {
     const skipInput = config?.skipInput
     const skipOutput = config?.skipOutput
     // Skip health checks by default
@@ -25,13 +25,32 @@ export function instrumentHttpServerWithRequestLogger(
     if (!skipInput && !skip) {
       inLogger[verbosity](inLog)
     }
-    return next().then(response => {
-      if (!skipOutput && !skip) {
+    let response: IHttpServerComponent.IResponse | undefined = undefined
+
+    try {
+      response = await next()
+      return response
+    } catch (e) {
+      // Craft a custom response with the purpose of printing the log
+      let statusCode = 500
+      if (typeof e === 'object' && e !== null && e !== undefined) {
+        if ('status' in e && typeof e.status == 'number') {
+          statusCode = e.status
+        } else if ('statusCode' in e && typeof e.statusCode == 'number') {
+          statusCode = e.statusCode
+        }
+      }
+      response = {
+        status: statusCode,
+        url: ctx.request.url
+      }
+      throw e
+    } finally {
+      if (!skipOutput && !skip && response) {
         outLogger[verbosity](
           config?.outputLog ? config.outputLog(ctx.request, response) : `[${ctx.request.method}: ${ctx.url.pathname}][${response.status}]`
         )
       }
-      return response
-    })
+    }
   })
 }
